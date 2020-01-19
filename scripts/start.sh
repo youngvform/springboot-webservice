d9f1cf2a9d3bdb95935169b98adbeb249073cba3
young-jinoh <austinhbk@naver.com>
2020-01-19 12:35:49 +0900

    scripts 내용 수저

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,17 +5,17 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=springboot-webservice
 
-echo "> build 파일 복사"
+echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR_NAME : $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
+
 echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
@@ -24,8 +24,7 @@ echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
-echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행한다. "
-
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -6,14 +6,13 @@ source ${ABSDIR}/profile.sh
 
 IDLE_PORT=$(find_idle_port)
 
-echo "> $IDLE_PORT 에서 구동중인 애플리케이션 PID 확인 "
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
 then
-  echo "> 구동중인 앱이 없으므로 종료하지 않음."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $IDLE_PID"
   kill -15 ${IDLE_PID}
   sleep 5
-fi
